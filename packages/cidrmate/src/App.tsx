cd2cad3b91e6ac893db2adc092b045d0ad0f5ae5
Marty Zalega <marty@zalega.me>
2025-07-27 12:28:34 +1000

    Use scopes to not clash with existing packages

--- packages/cidrmate/src/App.tsx
+++ packages/cidrmate/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { CopyContext, EmojiCycler, Field } from "shared";
+import { CopyContext, EmojiCycler, Field } from "@somethingmate/shared";
 import {
   isValidCidr,
   isValidIp,
